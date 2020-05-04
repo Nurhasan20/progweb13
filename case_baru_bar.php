f65668ec1ef0413b4674b9d925bb1bf085eac025
Nurhasan20 <61401487+Nurhasan20@users.noreply.github.com>
2020-05-04 11:14:38 +0700

    Add files via upload

---
+++ case_baru_bar.php
@@ -0,0 +1,49 @@
+<?php
+include('koneksi.php');
+$produk = mysqli_query($koneksi,"select * from negara");
+while($row = mysqli_fetch_array($produk)){
+	$nama_produk[] = $row['negara'];
+	
+	$query = mysqli_query($koneksi,"select sum(case_baru) as case_baru from covid where id_negara='".$row['id_negara']."'");
+	$row = $query->fetch_array();
+	$jumlah_produk[] = $row['case_baru'];
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat Grafik Menggunakan Chart JS</title>
+	<script type="text/javascript" src="Chart.js"></script>
+</head>
+<body>
+	<div style="width: 600px;height: 600px">
+		<canvas id="myChart"></canvas>
+	</div>
+	<script>
+		var ctx = document.getElementById("myChart").getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: <?php echo json_encode($nama_produk); ?>,
+				datasets: [{
+					label: 'Grafik Kenaikan Positif',
+					data: <?php echo json_encode($jumlah_produk); ?>,
+					backgroundColor: 'rgba(255, 99, 132, 0.2)',
+					borderColor: 'rgba(255,99,132,1)',
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+	</script>
+	</script>
+</body>
+</html>
